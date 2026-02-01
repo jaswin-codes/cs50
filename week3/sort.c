7246ecd32c246ab468e029df0d40eb235b077a2b
jaswin-codes <jaswinchinthala08@gmail.com>
2026-02-01 12:04:43 +0200

    Create sort.c

---
+++ week3/sort.c
@@ -0,0 +1,14 @@
+sort1 uses: Bubble Sort
+
+How do you know?: Very fast on sorted inputs because it finishes with 1 pass on best case scenario
+but slowest on random and reversed inputs because of n^2 in worst case scenarios.
+
+sort2 uses: Merge Sort
+
+How do you know?: Has the fastest times for random, reversed and sorted inputs because worst and best times are both n log n
+which are effective and faster.
+
+sort3 uses: Selection Sort
+
+How do you know?: Similar times for all 3 because best and worst case is still n^2 regardless of order.
+Slowest on sorted input since it still does full scan.
